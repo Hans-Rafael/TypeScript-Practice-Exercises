035f685097eabaee0b1a9d37a352f504a8db5f99
Hans <hansvekoni@gmail.com>
2025-02-06 11:42:04 -0300

    feat: Express and nodemon instaled

---
+++ 3_Advanced/11_BackEnd.ts
@@ -0,0 +1,5 @@
+/* Backend con Express:
+Crea un pequeño servidor Express con TypeScript que tenga un endpoint para recibir datos. */
+// first install experss y nodemon
+//npm install express
+//npm install --save-dev typescript @types/node @types/express ts-node nodemon
